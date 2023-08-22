d6bee93e82d63ca7be010f75cf7f23566b32e649
kahsayd <124149416+kahsayd@users.noreply.github.com>
2023-08-22 02:05:36 +0100

    Function playRound created that plays game
    It plays a single round of rock, paper and scissors against the
    computer.

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * compChoice.length);
     return compChoice[randomIndex];
 }
-console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+    let computerSelect = computerSelection;
+    let winResult = playerSelection + " beats " + computerSelect;
+    if (playerSelection === computerSelect) {
+        return "Draw " + playerSelection + " " + computerSelect + " stalemate"
+    }
+    else if (playerSelection === "Paper" && computerSelect === "Rock") {
+        return "You Win. " + winResult
+    }
+    else if (playerSelection === "Scissors" && computerSelect === "Paper") {
+        return "You Win. " + winResult
+    }
+    else if (playerSelection === "Rock" && computerSelect === "Scissors") {
+        return "You Win. " + winResult
+    }
+    else {
+        return "You Lose. " + playerSelection + " does not beat " + computerSelect
+    }
+}
+
+console.log(playRound("Rock", getComputerChoice()))
