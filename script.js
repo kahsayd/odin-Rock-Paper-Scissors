fd1c9620af7b0ff554bb01a8f2da4bdcb2193b4b
kahsayd <124149416+kahsayd@users.noreply.github.com>
2023-08-31 20:50:56 +0100

    Finished game.
    Added a scoreboard and a function that makes the game restart
    as soon as the computer or user wins/loses.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll('button');
 const resultsDiv = document.querySelector('#results');
+const scoreDiv = document.querySelector('#score');
 
 let playerPoints = 0;
 let computerPoints = 0;
@@ -11,30 +12,67 @@ function getComputerChoice() {
     return compChoice[randomIndex];
 }
 
+function displayResult(outputResult) {
+    resultsDiv.innerHTML = outputResult;
+}
+
+function displayScore() {
+    scoreDiv.innerHTML = "Player: " + playerPoints + " & " + "Computer: " + computerPoints;
+}
+
+function getPoints(result) {
+    if (playerPoints >= 5) {
+        displayResult("Congratulations. You've won the game!");
+        resetGame();
+    }
+    else if (computerPoints >= 5) {
+        displayResult("Computer wins. You've lost the game!");
+        resetGame();
+    }
+    else {
+        displayResult(result);
+    }
+}
+
 
 function playRound(playerSelection, computerSelection) {
     let computerSelect = computerSelection;
     let winResult = playerSelection + " beats " + computerSelect;
     let result = "";
+
     if (playerSelection === computerSelect) {
-        result = "Draw " + playerSelection + " " + computerSelect + " stalemate"
+        result = "Draw: " + playerSelection + " & " + computerSelect
+        playerPoints++;
     }
     else if (playerSelection === "Paper" && computerSelect === "Rock") {
         result = "You Win. " + winResult
+        playerPoints++;
     }
     else if (playerSelection === "Scissors" && computerSelect === "Paper") {
         result = "You Win. " + winResult
+        playerPoints++;
     }
     else if (playerSelection === "Rock" && computerSelect === "Scissors") {
         result = "You Win. " + winResult
+        playerPoints++;
     }
     else {
         result = "You Lose. " + playerSelection + " does not beat " + computerSelect
+        computerPoints++;
     }
+    
+    displayScore();
+
+    getPoints(result);
+
     return result;
 }
 
-// buttons is a node list. It looks and acts much like an array.
+function resetGame() {
+    playerPoints = 0;
+    computerPoints = 0;
+    displayScore();
+}
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
@@ -42,12 +80,5 @@ buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         const result = playRound(button.value, getComputerChoice());
-        displayResult(result);
     });
 });
-
-function displayResult(result) {
-    const resultParagraph = document.createElement('p');
-    resultParagraph.textContent = result;
-    resultsDiv.appendChild(resultParagraph);
-}
